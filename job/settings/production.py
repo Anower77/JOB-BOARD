89cd84d766aa7efd88c6469016ac4657a33130e0
anower77 <anowerhossain765562@gmail.com>
2024-12-29 17:52:07 +0600

    Update static files configuration

--- job/settings/production.py
+++ job/settings/production.py
@@ -17,12 +17,11 @@ DATABASES = {
     }
 }
 
-# Static files configuration for production
+# Static files configuration
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
 
 # Security settings
 CSRF_COOKIE_SECURE = True
-SESSION_COOKIE_SECURE = True
-SECURE_SSL_REDIRECT = True 
+SESSION_COOKIE_SECURE = True 
